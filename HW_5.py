5ee0fd7c793710922fef6b2e9dd8bf4a5fd5db1d
Olesia Boichuk <oleska.boychuk@gmail.com>
2021-03-08 01:35:38 +0200

    Slightly changed task 10

--- HW_5.py
+++ HW_5.py
@@ -242,9 +242,9 @@ class Student:
 
 student_1 = Student(1, "Kiki")
 email = "student@gmail.com"
-setattr(student_1, "email", "student@gmail.com")
+setattr(student_1, "student_email", "student@gmail.com")
 print(email)
-print(getattr(student_1, "email"))
+print(getattr(student_1, "student_email"))
 
 
 #11*.
